8b7e8041758b4a1cd1bf9c2e5bbb46cf033f0adb
JudithJessie <judithjessie12@gmail.com>
2023-02-24 17:30:59 +0300

    Functions and nested loop tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lower case alphabets
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,47 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to standard output
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropraitely.
+ */
+
+int _putchar(char c);
+
+/**
+ *print_alphabet - print lowercase alphabets
+ */
+
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 alphabets
+ */
+
+void print_alphabet_x10(void);
+
+int _islower (int c);
+
+int _isalpha (int c);
+
+int print_sign (int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+
+#endif
